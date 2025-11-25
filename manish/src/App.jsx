0b92b9407aba21963ec1d186a77a9bef9e3c5b1e
manish2224-08 <themanishchandel@gmail.com>
2025-11-25 22:20:32 +0530

    Update App.jsx

--- manish/src/App.jsx
+++ manish/src/App.jsx
@@ -5,7 +5,6 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { useEffect, useState } from "react";
 
 export default function App() {
   return (
